6ae076b48510635cd15d1c24253c3f919dc617eb
Chiaote Ni <aaron.chiao.19@gmail.com>
2024-06-13 00:03:42 +0800

    fix: stop using processImage(synchronously: true)
    - The processImage causes a dead lock under the structured concurrency enviroment

--- framework/Source/Operations/LookupFilter.swift
+++ framework/Source/Operations/LookupFilter.swift
@@ -1,15 +1,32 @@
+import Foundation
+
 public class LookupFilter: BasicOperation {
-    public var intensity:Float = 1.0 { didSet { uniformSettings["intensity"] = intensity } }
-    public var lookupImage:PictureInput? { // TODO: Check for retain cycles in all cases here
-        didSet {
-            lookupImage?.addTarget(self, atTargetIndex:1)
-            lookupImage?.processImage(synchronously: true)
-        }
+    public var intensity:Float = 1.0 {
+        didSet { uniformSettings["intensity"] = intensity }
     }
-    
+    public var lookupImage:PictureInput? { // TODO: Check for retain cycles in all cases here
+        didSet { lookupImage?.addTarget(self, atTargetIndex:1) }
+    }
+
+    private let accessQueue = DispatchQueue(label: "com.LookupFilter")
+
     public init() {
         super.init(fragmentFunctionName:"lookupFragment", numberOfInputs:2)
         
         ({intensity = 1.0})()
+    }
+
+    public override func newTextureAvailable(_ texture: Texture, fromSourceIndex: UInt) {
+        let function = super.newTextureAvailable
+        accessQueue.async { [weak self] in
+            guard let self else { return }
+            guard self.inputTextures[1] == nil, let lookupImage else {
+                function(texture, fromSourceIndex)
+                return
+            }
+            lookupImage.processImage() {
+                function(texture, fromSourceIndex)
+            }
+        }
     }
 }
