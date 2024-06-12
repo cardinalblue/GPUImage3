cba7620fdbadcc9bc91862ae67cc624cf6b07a42
Chiaote Ni <aaron.chiao.19@gmail.com>
2024-06-13 00:06:35 +0800

    refactor: modify the access control

--- framework/Source/OperationGroup.swift
+++ framework/Source/OperationGroup.swift
@@ -9,7 +9,7 @@ open class OperationGroup: ImageProcessingOperation {
     public init() {
     }
     
-    public func newTextureAvailable(_ texture:Texture, fromSourceIndex:UInt) {
+    open func newTextureAvailable(_ texture:Texture, fromSourceIndex:UInt) {
         inputImageRelay.newTextureAvailable(texture, fromSourceIndex:fromSourceIndex)
     }
     
