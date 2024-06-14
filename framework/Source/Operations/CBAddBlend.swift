f372ddfc78c9710c837ff6109857c7c8898602bc
ChiaoteNi <aaron.chiao.19@gmail.com>
2024-06-14 18:15:21 +0800

    fix: dead lock from process image (#20)
    # Description
    
    - The deadlock is caused by the sync version `newTexture()` inside the `PictureInput().processImage()`, which we invoke within the constructors or didSet for some filters ⬅️ This causes the deadlock when we init those filters under the async environment.
    
    - The fix is I remove all `processImage` invokes from the constructors or `didSet`, and then move them into the override `newTexture` inside GPUImage3
    
    - The new thing is that I just found out **the `DispatchSemaphore` has supported execution under the `async/await` environment**
      - `DispatchSemaphore` is confirmed `Sendable`, which is designed for the StructuredConcurrency, and is a kind of async-safe scoped locking
      - Using NSLock will cause a warning from Xcode and doesn't work as expected at runtime, but it does work well with DispatchSemaphore
    
    # Screenshots
    Before
    https://github.com/cardinalblue/pic-collage-ios/assets/40178645/d482fd6a-1697-442a-a959-bd0cb9f6e00f
    After
    https://github.com/cardinalblue/pic-collage-ios/assets/40178645/7bd524e5-fcd6-4db6-93d9-6084bf6bbe0d

---
+++ framework/Source/Operations/CBAddBlend.swift
@@ -0,0 +1,57 @@
+//
+//  CBAddBlend.swift
+//
+//
+//  Created by Chiaote Ni on 2024/6/14.
+//
+
+import Foundation
+
+// It's the same as the AddBlend in GPUImage.
+// To create a separate class because the original design of GPUImage is not for executing under the async/await environment, and will easily cause a deadlock.
+// For more details, see here: https://www.notion.so/piccollage/await-newTexture-3962c12cc694452faeaa8210760898f4
+// Therefore, to run the processImage with async, this class override the newTextureAvailable function to achieve this goal to avoid the deadlock.
+// Also, follow the implementation of LookupFilter, I create an blendImageInput for this filter, which allow us to use the same interface in PicCollage when we migrate from using GPUImage3 to using CBVisualEffectBuiltIn
+public class CBAddBlend: BasicOperation {
+
+    public var intensity: Float = 1 { didSet { uniformSettings["intensity"] = intensity } }
+
+    public var blendImageInput: PictureInput? {
+        willSet {
+            inputTextures.removeValue(forKey: 1)
+            blendImageInput?.removeAllTargets()
+        }
+        didSet {
+            blendImageInput?.addTarget(self, atTargetIndex: 1)
+        }
+    }
+
+    public init() {
+        super.init(fragmentFunctionName:"addBlendFragment", numberOfInputs:2)
+        ({
+            intensity = 1
+        })()
+    }
+
+    public override func newTextureAvailable(_ texture: Texture, fromSourceIndex: UInt) {
+        if fromSourceIndex == 1 {
+            inputTextures[fromSourceIndex] = texture
+            return
+        }
+
+        guard let blendImageInput, inputTextures[fromSourceIndex] == nil else {
+            super.newTextureAvailable(texture, fromSourceIndex: fromSourceIndex)
+            return
+        }
+
+        // DispatchSemaphore is Sendable and is a kind of async-safe scoped locking
+        // Using NSLock will cause a warning in Xcode and doesn't work as expected at runtime.
+        let lock = DispatchSemaphore(value: 0)
+        Task {
+            await blendImageInput.processImage()
+            super.newTextureAvailable(texture, fromSourceIndex: fromSourceIndex)
+            lock.signal()
+        }
+        lock.wait(timeout: .now() + 2)
+    }
+}
--- framework/Source/Operations/LookupFilter.swift
+++ framework/Source/Operations/LookupFilter.swift
@@ -1,15 +1,42 @@
+import Foundation
+
 public class LookupFilter: BasicOperation {
     public var intensity:Float = 1.0 { didSet { uniformSettings["intensity"] = intensity } }
     public var lookupImage:PictureInput? { // TODO: Check for retain cycles in all cases here
+        willSet {
+            inputTextures.removeValue(forKey: 1)
+            lookupImage?.removeAllTargets()
+        }
         didSet {
-            lookupImage?.addTarget(self, atTargetIndex:1)
-            lookupImage?.processImage(synchronously: true)
+            lookupImage?.addTarget(self, atTargetIndex: 1)
         }
     }
-    
+
     public init() {
         super.init(fragmentFunctionName:"lookupFragment", numberOfInputs:2)
         
         ({intensity = 1.0})()
+    }
+
+    public override func newTextureAvailable(_ texture: Texture, fromSourceIndex: UInt) {
+        if fromSourceIndex == 1 {
+            inputTextures[fromSourceIndex] = texture
+            return
+        }
+
+        guard let lookupImage, inputTextures[fromSourceIndex] == nil else {
+            super.newTextureAvailable(texture, fromSourceIndex: fromSourceIndex)
+            return
+        }
+
+        // DispatchSemaphore is Sendable and is a kind of async-safe scoped locking
+        // Using NSLock will cause a warning in Xcode and doesn't work as expected at runtime.
+        let lock = DispatchSemaphore(value: 0)
+        Task {
+            await lookupImage.processImage()
+            super.newTextureAvailable(texture, fromSourceIndex: fromSourceIndex)
+            lock.signal()
+        }
+        lock.wait(timeout: .now() + 2)
     }
 }
